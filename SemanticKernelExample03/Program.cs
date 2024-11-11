03cb6a94af901a1c492f7b46044922905a1fa0b7
Sergio Rondon <sergio19980816@gmail.com>
2024-11-10 21:01:22 -0500

    Agregar archivos de proyecto.

---
+++ SemanticKernelExample03/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using SemanticKernelExample03.Plugins;
+
+// Populate values from your OpenAI deployment
+var modelId = "phi3.5";
+var endpoint = "http://localhost:11434/";
+var apiKey = "123";
+
+// Create a kernel with Azure OpenAI chat completion
+var builder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
+
+// Build the kernel
+Kernel kernel = builder.Build();
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+// Add a plugin (the LightsPlugin class is defined below)
+kernel.Plugins.AddFromType<LightsPlugin>("Lights");
+
+// Enable planning
+#pragma warning disable SKEXP0001 // Este tipo se incluye solo con fines de evaluación y está sujeto a cambios o a que se elimine en próximas actualizaciones. Suprima este diagnóstico para continuar.
+OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+{
+    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+};
+#pragma warning restore SKEXP0001 // Este tipo se incluye solo con fines de evaluación y está sujeto a cambios o a que se elimine en próximas actualizaciones. Suprima este diagnóstico para continuar.
+
+// Create a history store the conversation
+var history = new ChatHistory();
+
+// Initiate a back-and-forth chat
+string? userInput;
+do
+{
+    // Collect user input
+    Console.Write("User > ");
+    userInput = Console.ReadLine();
+
+    // Add user input
+    history.AddUserMessage(userInput);
+
+    // Get the response from the AI
+    var result = await chatCompletionService.GetChatMessageContentAsync(
+        history,
+        executionSettings: openAIPromptExecutionSettings,
+        kernel: kernel);
+
+    // Print the results
+    Console.WriteLine("Assistant > " + result);
+
+    // Add the message from the agent to the chat history
+    history.AddMessage(result.Role, result.Content ?? string.Empty);
+} while (userInput is not null);
